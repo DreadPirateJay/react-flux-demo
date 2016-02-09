68abbb5433f9cec8cfc18011997252a900c1aaf6
Jay Earwood <jearwood@madg.com>
2016-02-09 14:51:54 -0600

    Home, Authors, and About pages w/ hacked in routing

---
+++ src/api/authorData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    authors: [
+        {
+            id: 'jearwood',
+            firstName: 'Jay',
+            lastName: 'Earwood'
+        },
+        {
+            id: 'bmullican',
+            firstName: 'Bryce',
+            lastName: 'Mullican'
+        },
+        {
+            id: 'rfarmer',
+            firstName: 'Ryan',
+            lastName: 'Farmer'
+        }
+    ]
+};
